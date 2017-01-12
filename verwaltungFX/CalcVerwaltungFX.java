0c4f2bb78bc2b838b56cf897c57b1b0c1d39ecd7
randwerArbeit <marzian@innosoft.de>
2017-01-12 19:21:32 +0100

    Beschriftungen und Inhalt in DB gekapselt

--- verwaltungFX/CalcVerwaltungFX.java
+++ verwaltungFX/CalcVerwaltungFX.java
@@ -39,7 +39,6 @@ public class CalcVerwaltungFX {
 			connection = DriverManager.getConnection("jdbc:sqlite:MovieDB.db");
 			Statement statement = connection.createStatement();
 		      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
 		      rs = statement.executeQuery("select * from film");
 		      
 		} catch (SQLException e) {
@@ -49,6 +48,85 @@ public class CalcVerwaltungFX {
 	      
 	  return connection; 
 	   
+	}
+	
+	public static String[] getEintragData(int id){
+		
+		String rueck = "";
+		
+		String[] rueckArray = new String[5];		
+		try {
+			Connection conn =connect();
+			Statement statement = conn.createStatement();
+			rs = statement.executeQuery("Select * from film where FilmID =" + Integer.toString(id+1));
+			rueckArray[0] = rs.getString("Titel");
+			rueckArray[1] = rs.getString("StoryLine");
+			rueckArray[2] =  rs.getString("ReleaseDate"); 
+			rueckArray[3] =  rs.getString("RunTimeMin");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rueckArray;
+	}
+	
+public static String[] getLabels(int SprachenID){
+		
+		String rueck = "";
+		int i = 0;
+		
+		String[] rueckArray = new String[10];		
+		try {
+			Connection conn =connect();
+			Statement statement = conn.createStatement();
+			rs = statement.executeQuery("Select * from Uebersetzung where SprachenID =" + Integer.toString(SprachenID));
+			
+			while(rs.next()){
+				rueckArray[i] = rs.getString("Bezeichnung");
+				i++;
+			}
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rueckArray;
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static Vector getLanguageVector() throws ClassNotFoundException, SQLException{
+		Connection conn =connect();
+		try {
+			conn = DriverManager.getConnection("jdbc:sqlite:MovieDB.db");
+			Statement statement = conn.createStatement();
+		      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+		      rs = statement.executeQuery("select * from film");
+		      
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+		
+		
+		
+		Vector rueck = new Vector();
+		while(rs.next()){
+			try {
+				rueck.add(rs.getString("Titel"));
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return rueck;	
 	}
 
 }
--- verwaltungFX/Controller.java
+++ verwaltungFX/Controller.java
@@ -1,11 +1,14 @@
 package verwaltungFX;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import verwaltung.CalcVerwaltung;
 
 public class Controller implements Initializable {
@@ -23,23 +26,34 @@ public class Controller implements Initializable {
 	private Label labelJahrInhalt;
 	@FXML
 	private Label labelStoryInhalt;
+	
+	
+	
+	
+	@SuppressWarnings("rawtypes")
+	@FXML
+	private ListView<String> filmListe;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		labelTitel.setText("Titel:");
-		labelJahr.setText("Erscheinungsjahrasdfasdfasdfsadfasdf:");
-		labelStory.setText("Story:");		
-		labelTitelInhalt.setText("test");
-		labelJahrInhalt.setText("test");
-		labelStoryInhalt.setText("test");
+		Vector x = new Vector();
 		
+		setLabels(1);
 		
 		try {
 			CalcVerwaltung.connect();
 			
 			
 			
-		} catch (ClassNotFoundException e) {
+			
+			
+			
+			
+			
+			x = CalcVerwaltungFX.getData();
+			filmListe.getItems().addAll(x);
+			
+		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -47,8 +61,38 @@ public class Controller implements Initializable {
 		System.out.println("alles geladen");
 	}
 	
-	public void beispielRoutineDieVerknuepftWerdenKann(){
+	public void beispielRoutineDieVerknuepftWerdenKann(){		
+		int index = filmListe.getSelectionModel().getSelectedIndex();
+		
+		String[] eintraegeArray = new String[5];	 
+		System.out.println(index);
+		eintraegeArray = CalcVerwaltungFX.getEintragData(index);
+		
+		labelTitelInhalt.setText(eintraegeArray[0]);	
+		labelJahrInhalt.setText(eintraegeArray[2]);
+		labelStoryInhalt.setText(eintraegeArray[1]);
+	}
+	
+	public void setLabels(int SprachenID){
+		String [] labels= new String[10];
+		labels = CalcVerwaltungFX.getLabels(SprachenID);
+		labelTitel.setText(labels[4]);
+		labelJahr.setText(labels[5]);
+		labelStory.setText(labels[6]);		
+		labelTitelInhalt.setText("test");
+		labelJahrInhalt.setText("test");
+		labelStoryInhalt.setText("test");
+		labelStoryInhalt.setWrapText(true);
+		
+		
+		
+		
+		
 		
 	}
-
+	
+	
+	
+	
+	
 }
